2f80f5a1be6bd77d2a909925804efd4e4530a614
yanzhangshuai <yanzhangshuai@126.com>
2019-06-11 13:28:27 +0800

    删除是否具有特性的扩展方法,因为此方法在System中存在

--- OrcasTeam.Shandard.Libary.Test/Extensions/Attribute/AttributeExtensionTest.cs
+++ OrcasTeam.Shandard.Libary.Test/Extensions/Attribute/AttributeExtensionTest.cs
@@ -1,21 +1,9 @@
-﻿using OrcasTeam.Shandard.Libary.Extensions;
-using OrcasTeam.Shandard.Libary.Test.Extensions.Attribute;
+﻿using OrcasTeam.Shandard.Libary.Test.Extensions.Attribute;
 using Xunit;
 
 namespace OrcasTeam.Shandard.Libary.Test.Extensions
 {
     public class AttributeExtensionTest
     {
-        [Fact]
-        public void HasAttributeTest()
-        {
-           // Assert.Throws<ArgumentException>(() => typeof(Student).IsDefined(typeof(NameAttribute)));
-            Assert.False(AttributeExtension.IsDefined<NameAttribute>(typeof(Student)));
-            Assert.True(AttributeExtension.IsDefined<NameAttribute>(typeof(Student), inherit: true));
-            Assert.False(new Student().IsDefined<NameAttribute>());
-            Assert.True(new Student().IsDefined<NameAttribute>(inherit: true));
-            Assert.False(typeof(Student).GetMethod("Method1").IsDefined<NameAttribute>());
-            Assert.True(AttributeExtension.IsDefined<NoAttribute>(typeof(Student), inherit: true));
-        }
     }
 }
